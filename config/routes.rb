8fd604d2df09aeb6a9a4e25aa9d0f31806e6d431
root <gontana1220@gmail.com>
2020-11-17 05:59:40 +0000

    記事作成　修正

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :users
   resources :articles, only:[:create,:destroy,]
   get '/articles', to: 'static_pages#home'
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
