24ba5ae53e0ee8150bcf254d856a01a3003f7484
gon tana <gontana1220@gmail.com>
2021-01-11 15:29:34 +0000

    ユーザー詳細画面と記事詳細画面のUI改善

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,7 +2,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_fill: [50, 50]
+  process resize_to_fill: [200, 200]
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
     storage :fog # 本番環境のみ
