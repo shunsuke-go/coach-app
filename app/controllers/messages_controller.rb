e3a34ea37976b677bc85a51707778d57d34a29d0
gon tana <gontana1220@gmail.com>
2021-01-15 09:29:47 +0000

    n+1問題解決＆その他UI微修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -26,33 +26,30 @@ class MessagesController < ApplicationController
   end
 
   def box
-    # 自分が所属するルームで自分以外のユーザが書いたメッセージが受信メッセージとなる。
     @user = User.find(params[:id])
+
+    # 自分が所属するルーム毎の最新メッセージ,作成日時と、メッセージ相手のid,名前,avatarのurlを取得する
     @new_send_messages = ActiveRecord::Base.connection.select_all("
-      SELECT m3.room_id,content,send_user_id, send_user_name,
-      partner_user_id, partner_user_name, partner_users_avatar,
-      m3.created_at
+      SELECT m3.room_id,content, partner_user_id,
+             partner_user_name, partner_users_avatar,m3.created_at
       FROM (
         SELECT m1.room_id AS room_id,
-          m1.content AS content,
-          m1.created_at AS created_at,
-          m1.user_id AS send_user_id,
-          users.id AS partner_user_id,
-          users.name AS partner_user_name,
-          users.avatar AS partner_users_avatar,
-          send_users.name AS send_user_name
+        m1.content AS content,
+        m1.created_at AS created_at,
+        users.id AS partner_user_id,
+        users.name AS partner_user_name,
+        users.avatar AS partner_users_avatar
         FROM messages m1
-          INNER JOIN entries ON entries.room_id = m1.room_id and entries.user_id != #{@user.id}
-          INNER JOIN users ON entries.user_id = users.id
-          INNER JOIN entries AS send_entries ON send_entries.room_id = m1.room_id and m1.user_id = send_entries.user_id
-          INNER JOIN users AS send_users ON send_entries.user_id = send_users.id
+        INNER JOIN entries ON entries.room_id = m1.room_id and entries.user_id != #{@user.id}
+        INNER JOIN users ON entries.user_id = users.id
       ) AS m3
       JOIN (SELECT room_id,MAX(created_at) AS created_at
-      FROM messages WHERE room_id
+      FROM messages
+      WHERE room_id
       IN (SELECT entries.room_id FROM entries WHERE entries.user_id = #{@user.id})
       GROUP BY room_id) AS m2
       ON m3.room_id = m2.room_id AND m3.created_at = m2.created_at
-     ").to_hash
+      ").to_hash
   end
 
   private
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,10 @@ class StaticPagesController < ApplicationController
     @user_ranks = User.where('ave_rate != 0 && review_count >= 5').order(ave_rate: 'DESC').limit(3)
     if logged_in?
       @article = current_user.articles.build
-      @feed_items = current_user.feed.paginate(page: params[:page], per_page: 5)
+      @feed_items = current_user.feed.with_rich_text_content.includes(
+        :user, :liked_users, :likes, :comments, :taggings, :tags
+      ).paginate(page: params[:page], per_page: 5)
+
       @likes = current_user.liked_articles  
     else
       @articles = Article.paginate(page: params[:page], per_page: 5)
