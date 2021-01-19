a33c8b3dc985ba1a6419933cf0ddc7ab179ffcab
gon tana <gontana1220@gmail.com>
2021-01-19 01:34:17 +0000

    記事検索機能追加完了

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,8 @@
 class SearchesController < ApplicationController
   def index
     @keyword = Article.ransack(params[:q])
-    @articles = @keyword.result
+    @articles = @keyword.result.with_rich_text_content.includes(
+      :user, :liked_users, :likes, :comments, :taggings, :tags
+    ).paginate(page: params[:page], per_page: 5)
   end
 end
