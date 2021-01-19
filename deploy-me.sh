abf0e33b310a388fc13c0ff58481876de5c4c0d6
gon tana <gontana1220@gmail.com>
2021-01-19 15:12:05 +0000

    production.rb　修正

--- deploy-me.sh
+++ deploy-me.sh
@@ -1,5 +1,5 @@
 cd /home/ec2-user/coach-app/  \
 && git pull origin main \
 && docker-compose -f docker-compose.prod.yml down \
-&& docker-compose -f docker-compose.prod.yml up -d --build 
+&& docker-compose -f docker-compose.prod.yml up -d --build \
 && docker system prune -f
