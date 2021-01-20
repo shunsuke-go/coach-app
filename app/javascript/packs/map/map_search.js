114acb508e091f02745f00c9c4a968fb0e10bf32
gon tana <gontana1220@gmail.com>
2021-01-20 13:12:00 +0000

    記事投稿のMap指定再修正

--- app/javascript/packs/map/map_search.js
+++ app/javascript/packs/map/map_search.js
@@ -1,8 +1,6 @@
 var button = document.getElementById('search_btn');
 var map_search = document.getElementById('map_search');
 var map_card = document.getElementById('map_card');
-//var balloon1 = document.getElementById('balloon1');
-
 button.addEventListener('click',(e) => {
   
   if(map_search.value == ""){
@@ -10,7 +8,7 @@ button.addEventListener('click',(e) => {
     if(document.querySelector('.balloon1')){
     }else{
       map_card.insertAdjacentHTML("afterbegin",
-      `<div class="balloon1" id="balloon1">
+      `<div class="balloon1" id="balloon1" style="color:red">
       <p>検索位置を入力してください</p>
       </div>`);
     }
