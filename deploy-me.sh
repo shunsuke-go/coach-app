42cee107c17268e158f3cb2a8e44bf991ad1dd02
gon tana <gontana1220@gmail.com>
2021-01-24 12:36:23 +0000

    デプロイファイルテスト4

--- deploy-me.sh
+++ deploy-me.sh
@@ -3,7 +3,5 @@ cd /home/ec2-user/coach-app/ \
 && docker system prune -af \
 && git pull origin main \
 && docker-compose -f docker-compose.prod.yml up -d --build \
-&& docker-compose -f docker-compose.prod.yml down \
-&& docker-compose -f docker-compose.prod.yml up -d \
-&& docker-compose -f docker-compose.prod.yml exec web rails assets:clobber RAILS_ENV=production \
-&& docker-compose -f docker-compose.prod.yml exec web rails assets:precompile RAILS_ENV=production
+&& docker exec coach-app_web_1 rails assets:clobber RAILS_ENV=production \
+&& docker exec coach-app_web_1 rails assets:precompile RAILS_ENV=production
