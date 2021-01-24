c006ef03496ad6b2f0639ea9c11d3d9f1d9bb474
gon tana <gontana1220@gmail.com>
2021-01-24 09:53:29 +0000

    デプロイファイル修正

--- deploy-me.sh
+++ deploy-me.sh
@@ -3,5 +3,5 @@ cd /home/ec2-user/coach-app/ \
 && docker system prune -af \
 && git pull origin main \
 && docker-compose -f docker-compose.prod.yml up -d --build \
-&& docker-compose -f docker-compose.prod.yml down \
-&& docker-compose -f docker-compose.prod.yml up -d
+&& docker-compose -f docker-compose.prod.yml run web rails assets:clobber RAILS_ENV=production \
+&& docker-compose -f docker-compose.prod.yml run web rails assets:precompile RAILS_ENV=production
