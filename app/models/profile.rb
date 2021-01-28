39943c9a638dd1b92c2bf6977cc78aa0eaad30f8
gon tana <gontana1220@gmail.com>
2021-01-28 09:45:07 +0000

    ユーザー権限周り修正

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,8 @@
 class Profile < ApplicationRecord
   belongs_to :user
-  validates :age, numericality: true, presence: true
-  validates :content, presence: true, length: { maximum: 1000 }
-  validates :address, presence: true, length: { maximum: 50 }
-  validates :wages, numericality: true, presence: true
+  validates :age, numericality: true, allow_nil: true
+  validates :content,  length: { maximum: 1000 }
+  validates :address,  length: { maximum: 50 }
+  validates :wages, numericality: true, allow_nil: true
   validates :favorite, length: { maximum: 50 }
 end
