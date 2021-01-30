7d1bf7ea18ddbd0056dc486c7c33b8596f7f3a9a
gon tana <gontana1220@gmail.com>
2021-01-30 07:37:14 +0000

    CSS微修正

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,5 +4,5 @@ class Profile < ApplicationRecord
   validates :content,  length: { maximum: 1000 }
   validates :address,  length: { maximum: 10 }
   validates :wages, numericality: { less_than_or_equal_to: 100_000 }, allow_nil: true
-  validates :favorite, length: { maximum: 20 }
+  validates :favorite, length: { maximum: 10 }
 end
