495c9c6761bbab11fafa04c45ea5a5140e53f4d3
gon tana <gontana1220@gmail.com>
2021-01-30 06:10:27 +0000

    fix-profiles

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,8 @@
 class Profile < ApplicationRecord
   belongs_to :user
-  validates :age, numericality: true, allow_nil: true
+  validates :age, numericality: { less_than_or_equal_to: 120 }, allow_nil: true
   validates :content,  length: { maximum: 1000 }
-  validates :address,  length: { maximum: 50 }
-  validates :wages, numericality: true, allow_nil: true
-  validates :favorite, length: { maximum: 50 }
+  validates :address,  length: { maximum: 10 }
+  validates :wages, numericality: { less_than_or_equal_to: 100000 }, allow_nil: true
+  validates :favorite, length: { maximum: 20 }
 end
