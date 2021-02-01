da09504136cc7803d49a9793ac8c07a94a9b164d
gon tana <gontana1220@gmail.com>
2021-02-01 09:52:20 +0000

    微修正

--- deploy-me.sh
+++ deploy-me.sh
@@ -2,4 +2,6 @@ cd /home/ec2-user/coach-app/ \
 && docker-compose -f docker-compose.prod.yml down \
 && git pull origin main \
 && docker-compose -f docker-compose.prod.yml up -d --build \
+&& docker-compose -f docker-compose.prod.yml down \
+&& docker-compose -f docker-compose.prod.yml up -d \
 && docker system prune -f
