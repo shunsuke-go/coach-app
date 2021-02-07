50f670f39719b7f04146a7605331f9f6a524df52
gon tana <gontana1220@gmail.com>
2021-02-07 14:28:40 +0000

    flashメッセージ2秒後消えるように改善

--- app/javascript/packs/materialize/header.js
+++ app/javascript/packs/materialize/header.js
@@ -3,4 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
   var instances = M.Sidenav.init(sidenav,[]);
   var dropdown = document.querySelectorAll('.dropdown-trigger');
   var instances = M.Dropdown.init(dropdown,[]);
+
+  var alert = document.getElementById('alert');
+  alert_remove = function(){
+    if(alert != null){
+    alert.remove()    
+  }            
+}
+  setTimeout(alert_remove,2000);
 });
