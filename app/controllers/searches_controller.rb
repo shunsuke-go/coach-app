7c5b960a28cdea883dea0d19f2571107f2065682
gon tana <gontana1220@gmail.com>
2021-02-23 09:47:40 +0000

    ランキングにも画像を付与

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,5 +4,10 @@ class SearchesController < ApplicationController
     @articles = @keyword.result.with_rich_text_content.includes(
       :user, :liked_users, :likes, :comments, :taggings, :tags
     ).paginate(page: params[:page], per_page: 5)
+    if @articles.empty?
+      gon.no_article = 1
+    else
+      gon.no_article = nil
+    end
   end
 end
