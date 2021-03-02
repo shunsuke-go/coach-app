9b98058d7ef74c7d1abffbcbaf2f6ebe8e06952f
gon tana <gontana1220@gmail.com>
2021-03-02 13:48:35 +0000

    rubocop自動整形

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,16 +1,13 @@
 class SearchesController < ApplicationController
-  def new
-  end
+  def new; end
 
   def index
     @keyword = Article.ransack(params[:q])
     @articles = @keyword.result.with_rich_text_content.includes(
       :user, :liked_users, :likes, :comments, :taggings, :tags
     ).paginate(page: params[:page], per_page: 5)
-    if @articles.empty?
-      gon.no_article = 1
-    else
-      gon.no_article = nil
-    end
+    gon.no_article = if @articles.empty?
+                       1
+                     end
   end
 end
