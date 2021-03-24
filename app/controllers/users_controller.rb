dfa1fd941176e3f99c1e2e7a9c8edbc53ffc02bd
gon tana <gontana1220@gmail.com>
2021-03-24 23:07:11 +0900

    バックエンドとフロントエンドの統合

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,22 +10,23 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-    @feed_items = @user.articles.with_rich_text_content.includes(
-      :user, :liked_users, :likes, :comments, :taggings, :tags
-    ).paginate(page: params[:page], per_page: 5)
-    if logged_in?
-      @room = @user.users_room(current_user)
-      if @room.blank?
-        @room = Room.new
-        @entry = Entry.new
-      end
-    end
+    redirect_to("#{ENV['FRONT_URL']}/users/#{params[:id]}")
+    # @user = User.find(params[:id])
+    # @feed_items = @user.articles.with_rich_text_content.includes(
+    #   :user, :liked_users, :likes, :comments, :taggings, :tags
+    # ).paginate(page: params[:page], per_page: 5)
+    # if logged_in?
+    #   @room = @user.users_room(current_user)
+    #   if @room.blank?
+    #     @room = Room.new
+    #     @entry = Entry.new
+    #   end
+    # end
 
-    @profile = Profile.find_by(user_id: params[:id])
-    @review = Review.new
-    @reviews = @user.passive_reviews
-    @ave_rate = @user.ave_rate.round(1) unless @user.ave_rate.nil?
+    # @profile = Profile.find_by(user_id: params[:id])
+    # @review = Review.new
+    # @reviews = @user.passive_reviews
+    # @ave_rate = @user.ave_rate.round(1) unless @user.ave_rate.nil?
   end
 
   def create
